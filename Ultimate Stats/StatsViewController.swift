b5112a6c4e962764fc5be165214089f70153817e
Alva Wei <alvawei@cs.washington.edu>
2017-05-24 14:40:40 -0700

    statsViewController, just needs firebase stuff now I think

--- Ultimate Stats/StatsViewController.swift
+++ Ultimate Stats/StatsViewController.swift
@@ -12,6 +12,8 @@ class StatsViewController: UIViewController, UITableViewDelegate, UITableViewDat
   
   @IBOutlet var tableView: UITableView!
   
+  let players = ["player1", "player2", "player3"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -26,7 +28,17 @@ class StatsViewController: UIViewController, UITableViewDelegate, UITableViewDat
   }
   
   func numberOfSections(in tableView: UITableView) -> Int {
-//    return 1
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return players.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "StatsCell", for: indexPath) as! StatsTableViewCell
+    cell.playerLabel.text = players[indexPath.row]
+    return cell
   }
   
   
