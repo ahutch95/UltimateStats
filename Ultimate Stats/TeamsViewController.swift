7026b2b2618544c021478352451a0a3962a7a576
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-05-23 19:08:49 -0700

    changed teams to controllerview and now pulls teams

--- Ultimate Stats/TeamsViewController.swift
+++ Ultimate Stats/TeamsViewController.swift
@@ -1,25 +1,70 @@
-//
-//  ViewController.swift
-//  Ultimate Stats
-//
-//  Created by iGuest on 5/15/17.
-//  Copyright © 2017 INFO 449. All rights reserved.
-//
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class TeamsViewController: UIViewController {
-
+class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var ref:FIRDatabaseReference!
+    var users: [String] = []
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ref = FIRDatabase.database().reference()
+        
+        
+        let userID = FIRAuth.auth()?.currentUser?.uid
+        ref = FIRDatabase.database().reference()
+        ref.child("users").child(userID!).child("teams").observeSingleEvent(of: .value, with: { (snapshot) in
+            var newItems: [String] = []
+            if !(snapshot.value is NSNull){
+                var value = (snapshot.value as? Array<String>)!
+                
+                for item in value{
+                    newItems.append(item)
+                }
+                self.tableView.delegate = self
+                self.tableView.dataSource = self
+                self.users = newItems
+                self.tableView.reloadData()
+            }
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    //creates new user
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! iQuizTableViewCell
+        let groceryItem = users[indexPath.row]
+        print("cell # \(indexPath.row) selected")
+        
+        cell.questionLabel.text = groceryItem
+        
+        
+        return cell
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // cell selected code here
+    }
 }
+
 
