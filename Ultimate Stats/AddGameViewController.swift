caa1de140e729a6b7e261b489d880ad0db66331c
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-05-24 09:31:53 -0700

    added addGame

---
+++ Ultimate Stats/AddGameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddGameViewController.swift
+//  Ultimate Stats
+//
+//  Created by studentuser on 5/24/17.
+//  Copyright © 2017 INFO 449. All rights reserved.
+//
+
+import UIKit
+class AddGameViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
